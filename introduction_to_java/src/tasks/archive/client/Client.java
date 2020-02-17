fde9a9b5c6fa2d9b2be83fccf6aeb3ac661b79e9
kvr4090 <58326274+kvr4090@users.noreply.github.com>
2020-02-17 14:12:52 +0300

    Update Client.java

--- introduction_to_java/src/tasks/archive/client/Client.java
+++ introduction_to_java/src/tasks/archive/client/Client.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 
 public class Client {
     private FileStudent fileStudent;
-    
+
     private void run() throws IOException {
         try {
             menu();
@@ -20,115 +20,118 @@ public class Client {
     }
 
     private void menu () throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("\nДля запроса дела, введите 1.\n" +
                 "Для добавления нового дела, введите 2.\n" +
                 "Для выхода, введите 3.\n" +
                 "Для остановки сервера, введите 4.\n");
-        String number = reader.readLine();
 
-        Pattern pattern = Pattern.compile("[1-4]");
-        Matcher matcher = pattern.matcher(number);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String number = reader.readLine();
 
-        if (matcher.find()) {
-            int temp = Integer.parseInt(number);
+            Pattern pattern = Pattern.compile("[1-4]");
+            Matcher matcher = pattern.matcher(number);
 
-            if (temp == 1) {
-                requestFileStudent();
+            if (matcher.find()) {
+                int temp = Integer.parseInt(number);
 
-                if (clientWork(fileStudent.getNumberOwner()).equals("empty")) {
-                    System.out.println("такого дела нет!");
+                if (temp == 1) {
+                    requestFileStudent();
 
-                } else {
-                    createFileStudent(clientWork(fileStudent.getNumberOwner()));
-                    System.out.println("Для просмотра дела, введите 1.\n" +
-                            "Для редактирования дела, введите 2.\n");
-                    String choice = reader.readLine();
+                    if (clientWork(fileStudent.getNumberOwner()).equals("empty")) {
+                        System.out.println("такого дела нет!");
 
-                    Pattern pattern1 = Pattern.compile("[1-2]");
-                    Matcher matcher1 = pattern1.matcher(choice);
+                    } else {
+                        createFileStudent(clientWork(fileStudent.getNumberOwner()));
+                        System.out.println("Для просмотра дела, введите 1.\n" +
+                                "Для редактирования дела, введите 2.\n");
+                        String choice = reader.readLine();
 
-                    if (matcher1.find()) {
-                        int asd = Integer.parseInt(choice);
+                        Pattern pattern1 = Pattern.compile("[1-2]");
+                        Matcher matcher1 = pattern1.matcher(choice);
 
-                        if (asd == 1) {
-                            toScreenFileStudent();
+                        if (matcher1.find()) {
+                            int asd = Integer.parseInt(choice);
+
+                            if (asd == 1) {
+                                toScreenFileStudent();
+                            } else {
+                                editFileStudentText();
+                                System.out.println(clientWork(fileStudent.getNumberOwner() + "|" + fileStudent.getText()));
+                            }
                         } else {
-                            editFileStudentText();
+                            System.out.println("Введите данные соответствующие требованиям!");
+                        }
+                    }
+                    menu();
+                } else if (temp == 2) {
+                    System.out.println("Для добавления дела, введите пароль (11 символов):");
+                    String pass = reader.readLine();
+                    Pattern pattern2 = Pattern.compile("[0-9]{11}");
+                    Matcher matcher2 = pattern2.matcher(pass);
+
+                    if (matcher2.find()) {
+                        if (clientWork(pass).equals("true")) {
+                            addFileStudent();
                             System.out.println(clientWork(fileStudent.getNumberOwner() + "|" + fileStudent.getText()));
+
+                        } else {
+                            System.out.println("Неверный пароль!");
                         }
                     } else {
-                        System.out.println("Введите данные соответствующие требованиям!");
+                        System.out.println("Введите пароль соответствующий требованиям!");
                     }
+                    menu();
+                } else if (temp == 3) {
+                    System.out.println("До свидания!");
+                } else if (temp == 4) {
+                    stopServer();
                 }
+            } else {
+                System.out.println("Ввели какую то дичь!");
                 menu();
-            } else if (temp == 2) {
-                System.out.println("Для добавления дела, введите пароль (11 символов):");
-                String pass = reader.readLine();
-                Pattern pattern2 = Pattern.compile("[0-9]{11}");
-                Matcher matcher2 = pattern2.matcher(pass);
-
-                if (matcher2.find()) {
-                    if (clientWork(pass).equals("true")) {
-                        addFileStudent();
-                        System.out.println(clientWork(fileStudent.getNumberOwner() + "|" + fileStudent.getText()));
-
-                    } else {
-                        System.out.println("Неверный пароль!");
-                    }
-                } else {
-                    System.out.println("Введите пароль соответствующий требованиям!");
-                }
-                menu();
-            } else if (temp == 3) {
-                System.out.println("До свидания!");
-            } else if (temp == 4) {
-                stopServer();
             }
-        } else {
-            System.out.println("Ввели какую то дичь!");
-            menu();
         }
-        reader.close();
     }
 
     private void stopServer() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Для остановки сервера, введите пароль (11 символов):");
-        String pass = reader.readLine();
-        Pattern pattern = Pattern.compile("[0-9]{11}");
-        Matcher matcher = pattern.matcher(pass);
 
-        if (matcher.find()) {
-            if (clientWork(pass).equals("true")) {
-                System.out.println(clientWork("stop"));
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String pass = reader.readLine();
+            Pattern pattern = Pattern.compile("[0-9]{11}");
+            Matcher matcher = pattern.matcher(pass);
 
+            if (matcher.find()) {
+                if (clientWork(pass).equals("true")) {
+                    System.out.println(clientWork("stop"));
+
+                } else {
+                    System.out.println("Неверный пароль!");
+                    menu();
+                }
             } else {
                 System.out.println("Неверный пароль!");
                 menu();
             }
-        } else {
-            System.out.println("Неверный пароль!");
-            menu();
         }
-        reader.close();
     }
 
     private void requestFileStudent() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         fileStudent = new FileStudent();
         System.out.println("Для запроса дела студента введите номер студента:");
-        String number = reader.readLine();
-        Pattern pattern = Pattern.compile("[0-9]{1,10}");
-        Matcher matcher = pattern.matcher(number);
 
-        if (matcher.find()) {
-            fileStudent.setNumberOwner(number);
-        } else {
-            System.out.println("Введите данные соответствующие требованиям!");
-            requestFileStudent();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String number = reader.readLine();
+            Pattern pattern = Pattern.compile("[0-9]{1,10}");
+            Matcher matcher = pattern.matcher(number);
+
+            if (matcher.find()) {
+                fileStudent.setNumberOwner(number);
+            } else {
+                System.out.println("Введите данные соответствующие требованиям!");
+                requestFileStudent();
+            }
         }
-        reader.close();
     }
 
     private void createFileStudent(String string) {
@@ -146,29 +149,31 @@ public class Client {
     }
 
     private void editFileStudentText() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Вводите требуемую информацию:");
-        String text = reader.readLine();
-        fileStudent.setText(text);
-        reader.close();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String text = reader.readLine();
+            fileStudent.setText(text);
+        }
     }
 
     private void addFileStudent() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Вводите номер студента");
-        String number = reader.readLine();
-        Pattern pattern = Pattern.compile("[0-9]{1,10}");
-        Matcher matcher = pattern.matcher(number);
 
-        if (matcher.find()) {
-            System.out.println("Вводите требуемую информацию о студенте:");
-            String test = reader.readLine();
-            fileStudent = new FileStudent(number, test);
-        } else {
-            System.out.println("Введите данные соответствующие требованиям!");
-            addFileStudent();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String number = reader.readLine();
+            Pattern pattern = Pattern.compile("[0-9]{1,10}");
+            Matcher matcher = pattern.matcher(number);
+
+            if (matcher.find()) {
+                System.out.println("Вводите требуемую информацию о студенте:");
+                String test = reader.readLine();
+                fileStudent = new FileStudent(number, test);
+            } else {
+                System.out.println("Введите данные соответствующие требованиям!");
+                addFileStudent();
+            }
         }
-        reader.close();
     }
 
     private String clientWork(String toServer) throws IOException {

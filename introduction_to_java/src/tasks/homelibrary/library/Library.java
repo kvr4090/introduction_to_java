54ccca66bbeb1263a7a3563efbfdb96fb6cce46b
kvr4090 <58326274+kvr4090@users.noreply.github.com>
2020-02-17 15:09:35 +0300

    Update Library.java

--- introduction_to_java/src/tasks/homelibrary/library/Library.java
+++ introduction_to_java/src/tasks/homelibrary/library/Library.java
@@ -58,57 +58,55 @@ public class Library {
     private void loadLibraryFromFile() throws IOException {
         library = new ArrayList<>();
         System.out.println("Введите директорию файла со списком книг:");
-        BufferedReader readerBooks = new BufferedReader(new InputStreamReader(System.in));
-        String filePath = readerBooks.readLine();
 
-        if (filePath.equals("")) {
-            System.out.println("Пустая строка!");
-            loadLibraryFromFile();
-        } else {
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader(new File(filePath)));
-                String line = reader.readLine();
+        try (BufferedReader readerBooks = new BufferedReader(new InputStreamReader(System.in))) {
+            String filePath = readerBooks.readLine();
 
-                while (line != null) {
-                    library.add(createBook(line));
-                    line = reader.readLine();
-                }
-            } catch (FileNotFoundException e) {
-                System.out.println("Указанный файл не найден!");
+            if (filePath.equals("")) {
+                System.out.println("Пустая строка!");
                 loadLibraryFromFile();
+            } else {
+                try (FileReader fileReader = new FileReader(new File(filePath));
+                     BufferedReader reader = new BufferedReader(fileReader)) {
+                     String line = reader.readLine();
+
+                     while (line != null) {
+                         library.add(createBook(line));
+                         line = reader.readLine();
+                     }
+                } catch (FileNotFoundException e) {
+                    System.out.println("Указанный файл не найден!");
+                    loadLibraryFromFile();
+                }
             }
         }
-        readerBooks.close();
     }
 
     private void loadUsersFromFile() throws IOException {
         users = new ArrayList<>();
         System.out.println("Введите директорию файла со списком пользователей:");
-        BufferedReader readerBooks = new BufferedReader(new InputStreamReader(System.in));
-        String filePath = readerBooks.readLine();
-        readerBooks.close();
 
-        if (filePath.equals("")) {
-            System.out.println("Пустая строка!");
-            loadUsersFromFile();
-        } else {
-            try {
-                FileReader fileReader = new FileReader(new File(filePath));
-                BufferedReader reader = new BufferedReader(fileReader);
-                String line = reader.readLine();
+        try (BufferedReader readerBooks = new BufferedReader(new InputStreamReader(System.in))) {
+            String filePath = readerBooks.readLine();
 
-                while (line != null) {
-                    users.add(createUser(line));
-                    line = reader.readLine();
-                }
-                reader.close();
-            } catch (FileNotFoundException e) {
-                System.out.println("Указанный файл не найден!");
+            if (filePath.equals("")) {
+                System.out.println("Пустая строка!");
                 loadUsersFromFile();
+            } else {
+                try (FileReader fileReader = new FileReader(new File(filePath));
+                     BufferedReader reader = new BufferedReader(fileReader)) {
+                    String line = reader.readLine();
+
+                    while (line != null) {
+                        users.add(createUser(line));
+                        line = reader.readLine();
+                    }
+                } catch (FileNotFoundException e) {
+                    System.out.println("Указанный файл не найден!");
+                    loadUsersFromFile();
+                }
             }
         }
-        fileReader.close();
-        reader.close();
     }
 
     private Book createBook(String string) {
@@ -127,42 +125,44 @@ public class Library {
     }
 
     private void saveLibraryToFile() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите директорию файла для сохранения списка книг:");
-        String filePath = reader.readLine();
-        reader.close();
+        
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String filePath = reader.readLine();
 
-        if (filePath.equals("")) {
-            System.out.println("Пустая строка!");
-            saveLibraryToFile();
-        } else {
-            writeUsingBufferedWriter(filePath);
+            if (filePath.equals("")) {
+                System.out.println("Пустая строка!");
+                saveLibraryToFile();
+            } else {
+                writeUsingBufferedWriter(filePath);
 
-            for (Book current: library) {
-                String appendBook = current.getAuthor() + "|" + current.getTitle() + "|" +
-                        current.getCountPages() + "|" +current.isPaper() + "|" + current.getDirectory();
+                for (Book current: library) {
+                    String appendBook = current.getAuthor() + "|" + current.getTitle() + "|" +
+                            current.getCountPages() + "|" +current.isPaper() + "|" + current.getDirectory();
 
-                appendUsingBufferedWriter(filePath, appendBook);
-            }
+                    appendUsingBufferedWriter(filePath, appendBook);
+                }
+            }   
         }
     }
 
     private void saveUsers() throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите директорию файла для сохранения списка пользователей:");
-        String filePath = reader.readLine();
-        reader.close();
+        
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String filePath = reader.readLine();
 
-        if (filePath.equals("")) {
-            System.out.println("Пустая строка!");
-            saveUsers();
-        } else {
-            writeUsingBufferedWriter(filePath);
+            if (filePath.equals("")) {
+                System.out.println("Пустая строка!");
+                saveUsers();
+            } else {
+                writeUsingBufferedWriter(filePath);
 
-            for (User current : users) {
-                String appendUser = current.getEmail() + "|" + new CaesarCipher().encrypt(current.getPassword());
-                appendUsingBufferedWriter(filePath, appendUser);
-            }
+                for (User current : users) {
+                    String appendUser = current.getEmail() + "|" + new CaesarCipher().encrypt(current.getPassword());
+                    appendUsingBufferedWriter(filePath, appendUser);
+                }
+            }   
         }
     }
 
@@ -207,52 +207,53 @@ public class Library {
     }
 
     private void initializationUser() throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("У вас уже есть учётная запись?(Да/Нет)");
-        String temp = reader.readLine();
+        
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String temp = reader.readLine();
 
-        if (temp.equals("")) {
-            System.out.println("Введена пустая строка!");
-            initializationUser();
+            if (temp.equals("")) {
+                System.out.println("Введена пустая строка!");
+                initializationUser();
 
-        } else if (temp.equals("Да")) {
-            System.out.println("Введите ваш email:");
-            String email = reader.readLine();
+            } else if (temp.equals("Да")) {
+                System.out.println("Введите ваш email:");
+                String email = reader.readLine();
 
-            if (checkEmail(email)) {
-                System.out.println("Введите пароль:");
-                String password = reader.readLine();
+                if (checkEmail(email)) {
+                    System.out.println("Введите пароль:");
+                    String password = reader.readLine();
 
-                if (checkPassword(email, password)) {
-                    System.out.println(email + " Добро пожаловать в библиотеку!");
-                    currentUser = new User(email, password);
+                    if (checkPassword(email, password)) {
+                        System.out.println(email + " Добро пожаловать в библиотеку!");
+                        currentUser = new User(email, password);
+                    } else {
+                        System.out.println("Неправильный пароль!");
+                        initializationUser();
+                    }
                 } else {
-                    System.out.println("Неправильный пароль!");
+                    System.out.println("Этот email не зарегистрирован.");
                     initializationUser();
                 }
-            } else {
-                System.out.println("Этот email не зарегистрирован.");
-                initializationUser();
-            }
 
-        } else if (temp.equals("Нет")) {
-            System.out.println("Введите ваше имя:");
-            String name = reader.readLine();
+            } else if (temp.equals("Нет")) {
+                System.out.println("Введите ваше имя:");
+                String name = reader.readLine();
 
-            if (name.equals("")) {
-                System.out.println("Пустая строка!");
-                initializationUser();
+                if (name.equals("")) {
+                    System.out.println("Пустая строка!");
+                    initializationUser();
+                } else {
+                    User current = new User(name, users);
+                    users.add(current);
+                    currentUser = current;
+                }
             } else {
-                User current = new User(name, users);
-                users.add(current);
-                currentUser = current;
-            }
-        } else {
-            initializationUser();
+                initializationUser();
+            }   
         }
-        reader.close();
     }
-
+    
     private boolean checkEmail(String email) {
         for (User current : users) {
             if (current.getEmail().equals(email)) {
@@ -273,13 +274,14 @@ public class Library {
 
     private void adminMenu() throws Exception {
         int value = 0;
-
+        String temp;
         System.out.println("\nДля добавления книги введите 1\n" +
                 "Для удаления книги введите 2 и книгу для удаления\n" +
                 "Для перехода в пользовательский режим введите 3\n");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String temp = reader.readLine();
-        reader.close();
+        
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            temp = reader.readLine();
+        }
 
         if (temp.equals("")) {
             System.out.println("Введена пустая строка!");
@@ -314,7 +316,7 @@ public class Library {
 
     private void userMenu() throws Exception {
         int value = 0;
-
+        String temp;
         System.out.println("\nДля просмотра списка книг введите 1 (Для просмотра следующих страниц нажимайте Enter)\n" +
                 "Для поиска книги по автору, введите 2\n" +
                 "Для поиска книги по названию, введите 3\n" +
@@ -323,70 +325,71 @@ public class Library {
                 "Для смены пароля введите 6\n" +
                 "Для выхода введите 7\n" +
                 "Для перехода в режим Администратора, введите 8\n");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String temp = reader.readLine();
 
-        if (temp.equals("")) {
-            System.out.println("Введена пустая строка!");
-            userMenu();
-
-        } else {
-            Pattern patternCost = Pattern.compile("[1-8]");
-            Matcher matcher = patternCost.matcher(temp);
-
-            if (matcher.find()) {
-                value = Integer.parseInt(temp);
-            } else {
-                System.out.println("Введены недопустимые символы!");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            temp = reader.readLine();
+            
+            if (temp.equals("")) {
+                System.out.println("Введена пустая строка!");
                 userMenu();
-            }
-        }
 
-        if (value == 1) {
-            currentUser.browseLibrary(library);
-            userMenu();
+            } else {
+                Pattern patternCost = Pattern.compile("[1-8]");
+                Matcher matcher = patternCost.matcher(temp);
 
-        } else if (value == 2) {
-            currentUser.searchBookWithAuthor(library);
-            userMenu();
-
-        } else if (value == 3) {
-            currentUser.searchBookWithTitle(library);
-            userMenu();
-
-        } else if (value == 4) {
-            currentUser.searchBookWithAuthorAndTitle(library);
-            userMenu();
-
-        } else if (value == 5) {
-            currentUser.userOfferBook(admin, currentUser);
-            userMenu();
-
-        } else if (value == 6) {
-
-            for (User current : users) {
-                if (current.getEmail().equals(currentUser.getEmail())) {
-                    currentUser.setWithUserPassword();
-                    current.setPassword(currentUser.getPassword());
+                if (matcher.find()) {
+                    value = Integer.parseInt(temp);
+                } else {
+                    System.out.println("Введены недопустимые символы!");
+                    userMenu();
                 }
             }
-            userMenu();
 
-        } else if (value == 7) {
-            System.out.println("Не забудьте корректно указать директории файлов сохранения данных.");
-
-        } else if (value == 8) {
-            System.out.println("Введите пароль:");
-            temp = reader.readLine();
-
-            if (temp.equals(admin.getPassword())) {
-                adminMenu( );
-
-            } else {
-                System.out.println("Пароль неверный!");
+            if (value == 1) {
+                currentUser.browseLibrary(library);
                 userMenu();
+
+            } else if (value == 2) {
+                currentUser.searchBookWithAuthor(library);
+                userMenu();
+
+            } else if (value == 3) {
+                currentUser.searchBookWithTitle(library);
+                userMenu();
+
+            } else if (value == 4) {
+                currentUser.searchBookWithAuthorAndTitle(library);
+                userMenu();
+
+            } else if (value == 5) {
+                currentUser.userOfferBook(admin, currentUser);
+                userMenu();
+
+            } else if (value == 6) {
+
+                for (User current : users) {
+                    if (current.getEmail().equals(currentUser.getEmail())) {
+                        currentUser.setWithUserPassword();
+                        current.setPassword(currentUser.getPassword());
+                    }
+                }
+                userMenu();
+
+            } else if (value == 7) {
+                System.out.println("Не забудьте корректно указать директории файлов сохранения данных.");
+
+            } else if (value == 8) {
+                System.out.println("Введите пароль:");
+                temp = reader.readLine();
+
+                if (temp.equals(admin.getPassword())) {
+                    adminMenu( );
+
+                } else {
+                    System.out.println("Пароль неверный!");
+                    userMenu();
+                }
             }
         }
-        reader.close();
     }
 }
